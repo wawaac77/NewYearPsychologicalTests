53aef7fedba56ab400944af701ee50fdab32f2f6
Alice Jin <alice.c.jin@pccw.com>
2018-01-08 14:33:30 +0800

    Initial Commit

---
+++ NewYearPsychologicalTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewYearPsychologicalTests
+//
+//  Created by Alice Jin on 8/1/2018.
+//  Copyright © 2018 Alice Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewYearPsychologicalTests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewYearPsychologicalTests
+//
+//  Created by Alice Jin on 8/1/2018.
+//  Copyright © 2018 Alice Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
